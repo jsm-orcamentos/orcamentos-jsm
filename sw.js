b79cf7c7acc69343048442429b96ac6300be0320
jsm-orcamentos <jsm.seguranca.contatos@gmail.com>
2026-02-05 22:47:45 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,34 @@
-const CACHE_NAME = 'jsm-orcamentos-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/logo.png',
-  '/manifest.json'
+const CACHE_NAME = "jsm-orcamentos-v1";
+
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "./logo.png",
+  "./manifest.json"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((k) => k !== CACHE_NAME)
+          .map((k) => caches.delete(k))
+      )
+    )
   );
+});
 
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
 });
