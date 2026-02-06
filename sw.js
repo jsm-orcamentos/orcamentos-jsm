64c21cdb716e733d003bbd884560a9356bad18bc
jsm-orcamentos <jsm.seguranca.contatos@gmail.com>
2026-02-05 22:39:01 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -18,24 +18,5 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
   );
-});const CACHE_NAME = 'jsm-orcamentos-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/logo.png',
-  '/manifest.json'
-];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
 });
